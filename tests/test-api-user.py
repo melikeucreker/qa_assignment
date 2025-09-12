45ea37517f04c28b576874f8c30d9e5e46b1a7d9
Melike Eker <melikeucareker@gmail.com>
2025-09-12 23:53:03 +0300

    fix:user creation test has been changed

--- tests/test-api-user.py
+++ tests/test-api-user.py
@@ -5,26 +5,26 @@ from main import app
 @pytest.fixture
 def client():
     return TestClient(app)
+
 class TestUserAPI:
-    def test_user_creation(self,client):
+    def test_user_creation(self, client):
         payload = {
-
-                "username": "melike",
-                "email": "meliketest+test1@example.com",
-                "password": "aB12",
-                "age": 18,
-                "phone": "4555673723"
-
+            "username": "melike",
+            "email": "meliketest+test1@example.com",
+            "password": "aB123456",
+            "age": 18,
+            "phone": "4555673723"
         }
 
-       #Positive Case
         response = client.post("/users/", json=payload)
 
-        # Asserts
+
+        print(response.status_code)
+        print(response.json())
+
         assert response.status_code == 201
         data = response.json()
         assert data["username"] == payload["username"]
         assert data["email"] == payload["email"]
-        assert data["password"] == payload["password"]
         assert data["age"] == payload["age"]
         assert data["phone"] == payload["phone"]
